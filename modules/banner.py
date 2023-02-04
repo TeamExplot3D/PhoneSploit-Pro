05903b1461b6aacdda418bff74ad2778f84ed2f4
Team Explot3D <36406639+TeamExplot3D@users.noreply.github.com>
2023-02-04 01:26:15 -0500

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,130 +1,52 @@
-from modules import color
-banner1 = 
-menu1 = f'''
 
+from modules import color
+
+menu1 = f'''
     {color.WHITE}1. {color.GREEN}Conectar un dispositivo              {color.WHITE}6. {color.GREEN}Obtener captura de pantalla                        {color.WHITE}11. {color.GREEN}Instalar un APK
     {color.WHITE}2. {color.GREEN}Lista de dispositivos conectados     {color.WHITE}7. {color.GREEN}Grabación de pantalla                              {color.WHITE}12. {color.GREEN}Desinstalar una aplicación
     {color.WHITE}3. {color.GREEN}Desconectar todos los dispositivos   {color.WHITE}8. {color.GREEN}Descargar archivo/carpeta desde el dispositivo     {color.WHITE}13. {color.GREEN}Lista de aplicaciones instaladas
     {color.WHITE}4. {color.GREEN}Shell de dispositivo de acceso       {color.WHITE}9. {color.GREEN}Enviar archivo/carpeta al dispositivo              {color.WHITE}14. {color.GREEN}Reiniciar dispositivo
     {color.WHITE}5. {color.GREEN}Espejo y dispositivo de control      {color.WHITE}10. {color.GREEN}Ejecutar una aplicación                           {color.WHITE}15. {color.GREEN}Hackear dispositivo {color. RED}(Usando Metasploit)
-
-
    {color.YELLOW} 
   N : Next Page                                      (Page : 1 / 3)'''
 
 menu2 = f'''
-
     {color.WHITE}16. {color.GREEN}Enumerar todas las carpetas/archivos   {color.WHITE}21. {color.GREEN}Captura de pantalla anónima                     {color.WHITE}26. {color.GREEN}Reproducir un vídeo en el dispositivo
     {color.WHITE}17. {color.GREEN}Opciones avanzadas de reinicio         {color.WHITE}22. {color.GREEN}Registro de pantalla anónimo                    {color.WHITE}27. {color.GREEN}Obtener información del dispositivo
     {color.WHITE}18. {color.GREEN}Copiar datos de WhatsApp               {color.WHITE}23. {color.GREEN}Abrir un enlace en el dispositivo               {color.WHITE}28. {color.GREEN}Obtenga información sobre la batería
     {color.WHITE}19. {color.GREEN}Copiar todas las capturas de pantalla  {color.WHITE}24. {color.GREEN}Mostrar una foto en el dispositivo              {color.WHITE}29. {color.GREEN}Usar códigos clave (dispositivo de control)
     {color.WHITE}20. {color.GREEN}Copiar todas las fotos de la cámara    {color.WHITE}25. {color.GREEN}Reproducir un audio en el dispositivo           {color.WHITE}30. {color.GREEN}Enviar SMS
-
-
    {color.YELLOW} 
   P : Previous Page         N : Next Page            (Page : 2 / 3)'''
 
 menu3 = f'''
-
     {color.WHITE}31. {color.GREEN}Desbloquear dispositivo             {color.WHITE}36. {color.GREEN}Extraer el APK de la aplicación instalada   {color.WHITE}    {color.GREEN}
     {color.WHITE}32. {color.GREEN}Bloquear dispositivo                {color.WHITE}37. {color.GREEN}Detener CICPC Infiltrador                   {color.WHITE}    {color.GREEN}
     {color.WHITE}33. {color.GREEN}Volcar todos los SMS                {color.WHITE}    {color.GREEN}                                            {color.WHITE}    {color.GREEN}
     {color.WHITE}34. {color.GREEN}Volcar todos los contactos          {color.WHITE}    {color.GREEN}                                            {color.WHITE}    {color.GREEN}
     {color.WHITE}35. {color.GREEN}Volcar registros de llamadas        {color.WHITE}    {color.GREEN}                                             {color.WHITE}    {color.GREEN}
-
-
    {color.YELLOW} 
   P : Previous Page                                  (Page : 3 / 3)'''
 
 menu = [menu1, menu2, menu3]
 
 instruction = f'''
-
 Este ataque lanzará Metasploit-Framework (msfconsole)
-
 Use 'Ctrl + C' para detenerse en cualquier punto
-
 1. Espere hasta que vea:
     
-{color. VERDE}meterpreter > {color. BLANCO}
-
+{color. GREEN}meterpreter > {color. WHITE}
 2. Luego use el comando 'help' para ver todos los comandos meterpreter:
-
-{color. VERDE}meterpreter > {color. AMARILLO}ayuda {color. BLANCO}
-
+{color. GREEN}meterpreter > {color. YELLOW}ayuda {color. WHITE}
 3. Para salir de meterpreter ingrese 'exit' o Para salir de Metasploit ingrese 'exit -y':
-
-{color. VERDE}meterpreter > {color. AMARILLO}exit {color. BLANCO}
-
-{color. VERDE}msf6 > {color. AMARILLO}exit -y {color. BLANCO}
+{color. GREEN}meterpreter > {color. YELLOW}exit {color. WHITE}
+{color. GREEN}msf6 > {color. YELLOW}exit -y {color. WHITE}
      
-{color. ROJO}[CICPC INFILTRADOR] {color. BLANCO}Presione 'Enter' para continuar el ataque / '0' para volver al menú principal
+{color. RED}[CICPC INFILTRADOR] {color. WHITE}Presione 'Enter' para continuar el ataque / '0' para volver al menú principal
     '''
 
-banner2 = 
-from modules import color
-menu1 = f'''
+banner2 = f'''
 
-    {color.WHITE}1. {color.GREEN}Conectar un dispositivo              {color.WHITE}6. {color.GREEN}Obtener captura de pantalla                        {color.WHITE}11. {color.GREEN}Instalar un APK
-    {color.WHITE}2. {color.GREEN}Lista de dispositivos conectados     {color.WHITE}7. {color.GREEN}Grabación de pantalla                              {color.WHITE}12. {color.GREEN}Desinstalar una aplicación
-    {color.WHITE}3. {color.GREEN}Desconectar todos los dispositivos   {color.WHITE}8. {color.GREEN}Descargar archivo/carpeta desde el dispositivo     {color.WHITE}13. {color.GREEN}Lista de aplicaciones instaladas
-    {color.WHITE}4. {color.GREEN}Shell de dispositivo de acceso       {color.WHITE}9. {color.GREEN}Enviar archivo/carpeta al dispositivo              {color.WHITE}14. {color.GREEN}Reiniciar dispositivo
-    {color.WHITE}5. {color.GREEN}Espejo y dispositivo de control      {color.WHITE}10. {color.GREEN}Ejecutar una aplicación                           {color.WHITE}15. {color.GREEN}Hackear dispositivo {color. RED}(Usando Metasploit)
-
-
-   {color.YELLOW} 
-  N : Next Page                                      (Page : 1 / 3)'''
-
-menu2 = f'''
-
-    {color.WHITE}16. {color.GREEN}Enumerar todas las carpetas/archivos   {color.WHITE}21. {color.GREEN}Captura de pantalla anónima                     {color.WHITE}26. {color.GREEN}Reproducir un vídeo en el dispositivo
-    {color.WHITE}17. {color.GREEN}Opciones avanzadas de reinicio         {color.WHITE}22. {color.GREEN}Registro de pantalla anónimo                    {color.WHITE}27. {color.GREEN}Obtener información del dispositivo
-    {color.WHITE}18. {color.GREEN}Copiar datos de WhatsApp               {color.WHITE}23. {color.GREEN}Abrir un enlace en el dispositivo               {color.WHITE}28. {color.GREEN}Obtenga información sobre la batería
-    {color.WHITE}19. {color.GREEN}Copiar todas las capturas de pantalla  {color.WHITE}24. {color.GREEN}Mostrar una foto en el dispositivo              {color.WHITE}29. {color.GREEN}Usar códigos clave (dispositivo de control)
-    {color.WHITE}20. {color.GREEN}Copiar todas las fotos de la cámara    {color.WHITE}25. {color.GREEN}Reproducir un audio en el dispositivo           {color.WHITE}30. {color.GREEN}Enviar SMS
-
-
-   {color.YELLOW} 
-  P : Previous Page         N : Next Page            (Page : 2 / 3)'''
-
-menu3 = f'''
-
-    {color.WHITE}31. {color.GREEN}Desbloquear dispositivo             {color.WHITE}36. {color.GREEN}Extraer el APK de la aplicación instalada   {color.WHITE}    {color.GREEN}
-    {color.WHITE}32. {color.GREEN}Bloquear dispositivo                {color.WHITE}37. {color.GREEN}Detener CICPC Infiltrador                   {color.WHITE}    {color.GREEN}
-    {color.WHITE}33. {color.GREEN}Volcar todos los SMS                {color.WHITE}    {color.GREEN}                                            {color.WHITE}    {color.GREEN}
-    {color.WHITE}34. {color.GREEN}Volcar todos los contactos          {color.WHITE}    {color.GREEN}                                            {color.WHITE}    {color.GREEN}
-    {color.WHITE}35. {color.GREEN}Volcar registros de llamadas        {color.WHITE}    {color.GREEN}                                             {color.WHITE}    {color.GREEN}
-
-
-   {color.YELLOW} 
-  P : Previous Page                                  (Page : 3 / 3)'''
-
-menu = [menu1, menu2, menu3]
-
-instruction = f'''
-
-Este ataque lanzará Metasploit-Framework (msfconsole)
-
-Use 'Ctrl + C' para detenerse en cualquier punto
-
-1. Espere hasta que vea:
-    
-{color. VERDE}meterpreter > {color. BLANCO}
-
-2. Luego use el comando 'help' para ver todos los comandos meterpreter:
-
-{color. VERDE}meterpreter > {color. AMARILLO}ayuda {color. BLANCO}
-
-3. Para salir de meterpreter ingrese 'exit' o Para salir de Metasploit ingrese 'exit -y':
-
-{color. VERDE}meterpreter > {color. AMARILLO}exit {color. BLANCO}
-
-{color. VERDE}msf6 > {color. AMARILLO}exit -y {color. BLANCO}
-     
-{color. ROJO}[CICPC INFILTRADOR] {color. BLANCO}Presione 'Enter' para continuar el ataque / '0' para volver al menú principal
-    '''
-
-banner2 = 
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \
@@ -132,8 +54,8 @@ banner2 =
 
             {color.RED}v1.3{color.WHITE}            {color.WHITE}Por: Will
 '''
-
 banner3 = f'''
+
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \
@@ -141,9 +63,9 @@ banner3 = f'''
 
             {color.RED}v1.3{color.WHITE}             {color.WHITE}Por: Will 
 '''
-
 banner4 = f'''
-  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
+
+ _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \
 
@@ -151,23 +73,24 @@ banner4 = f'''
         {color.RED}v1.3{color.WHITE}                             {color.WHITE}By github.com/AzeemIdrisi
 '''
 banner5 = f'''
-     _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
+
+ _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \                                                               
 
         {color.RED}v1.3{color.WHITE}        {color.WHITE}Por: Will
 '''
-
 banner6 = f'''
-  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
+
+ _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \                                                         
     
            {color.RED}v1.3{color.WHITE}               {color.WHITE}Por: Will 
 
 '''
-
 banner10 = f'''
+
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \   
@@ -176,8 +99,8 @@ banner10 = f'''
             {color.RED}v1.3{color.WHITE}                               
 
 '''
-
 banner11 = f'''
+
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \   
@@ -188,8 +111,8 @@ banner11 = f'''
             {color.RED}v1.3{color.WHITE}                            
 
 '''
-
 banner12 = f'''
+
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
 \_ _|_ \_ |   \_   |  | \_/  \/  _|_ |_   _|_ | \| |  _|_ |_  |  | \ /--\ |_/ \_/ | \   
@@ -202,7 +125,6 @@ banner12 = f'''
 '''
 banner_list = [banner2, banner3, banner4, banner5,
                banner6, banner10, banner11, banner12]
-
 instructions_banner = f'''{color.CYAN}
         ____           __                                
        /  _/___  _____/ /________  _______
@@ -211,8 +133,7 @@ instructions_banner = f'''{color.CYAN}
     /___/_/ /_/____/\__/_/   \__,_/\___/
         {color.WHITE}                                                        
 '''
-
-hacking_banner = f'''{color.GREEN}
+hacking_banner = f'''
     
  _ ___  _  _   _         _       ___      ___       _ ___    ___  _        _   _   _  
 /   |  /  |_) /    |\/| / \ \  /  |  |     |  |\ | |_  |  |   |  |_)  /\  | \ / \ |_) 
@@ -221,16 +142,14 @@ hacking_banner = f'''{color.GREEN}
 
     {color.WHITE}
 '''
-
 keycode_menu = f'''
-    {color.WHITE}1. {color.GREEN}Keyboard Text Input                {color.WHITE}11. {color.GREEN}Enter
-    {color.WHITE}2. {color.GREEN}Home                               {color.WHITE}12. {color.GREEN}Volume Up
-    {color.WHITE}3. {color.GREEN}Back                               {color.WHITE}13. {color.GREEN}Volume Down          
-    {color.WHITE}4. {color.GREEN}Recent Apps                        {color.WHITE}14. {color.GREEN}Media Play           
-    {color.WHITE}5. {color.GREEN}Power Button                       {color.WHITE}15. {color.GREEN}Media Pause
-    {color.WHITE}6. {color.GREEN}DPAD Up                            {color.WHITE}16. {color.GREEN}Tab 
-    {color.WHITE}7. {color.GREEN}DPAD Down                          {color.WHITE}17. {color.GREEN}Esc
-    {color.WHITE}8. {color.GREEN}DPAD Left           
-    {color.WHITE}9. {color.GREEN}DPAD Right
-   {color.WHITE}10. {color.GREEN}Delete/Backspace
-'''
+    {color.WHITE}1. {color.GREEN}Entrada Por Teclado               {color.WHITE}11. {color.GREEN}Enter
+    {color.WHITE}2. {color.GREEN}Casa                               {color.WHITE}12. {color.GREEN}Volumen Up
+    {color.WHITE}3. {color.GREEN}Atras                               {color.WHITE}13. {color.GREEN}Volumen Down          
+    {color.WHITE}4. {color.GREEN}Apps Recientes                        {color.WHITE}14. {color.GREEN}Media Play           
+    {color.WHITE}5. {color.GREEN}Boton Power                       {color.WHITE}15. {color.GREEN}Media Pausa
+    {color.WHITE}6. {color.GREEN}DPAD Arriba                            {color.WHITE}16. {color.GREEN}Tab 
+    {color.WHITE}7. {color.GREEN}DPAD Abajo                          {color.WHITE}17. {color.GREEN}Esc
+    {color.WHITE}8. {color.GREEN}DPAD Izquierda           
+    {color.WHITE}9. {color.GREEN}DPAD Derecha
+   {color.WHITE}10. {color.GREEN}Suprimir/Backspace '''
